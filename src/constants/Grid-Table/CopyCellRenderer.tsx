cfd40c9d0cba0eb8409e594406c8fd49cb4d50ea
Muzammil Aly <muzammilnizaami9@gmail.com>
2026-02-20 14:54:34 +0500

    feat: Add URL field to order items and update CopyCellRenderer for URL handling

--- src/constants/Grid-Table/CopyCellRenderer.tsx
+++ src/constants/Grid-Table/CopyCellRenderer.tsx
@@ -105,7 +105,11 @@ const CopyCellRenderer: React.FC<ICellRendererParams> = (props) => {
     }
   };
 
-  const isOrderUrl = colDef?.field === "order_url" && value && value !== "N/A";
+  // const isOrderUrl = colDef?.field === "order_url" && value && value !== "N/A";
+  const isOrderUrl =
+    (colDef?.field === "order_url" || colDef?.field === "url") &&
+    value &&
+    value !== "N/A";
 
   return (
     <div
